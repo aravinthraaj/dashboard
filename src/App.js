f24c529c6c6c27038a86d37a35d7037cb82251ba
aravinthraaj <aravinthraajk@gmail.com>
2022-01-23 00:44:23 +0530

    Topbar and SideBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
-import "./App.css"
+import Topbar from "./components/Topbar/Topbar"
+import "./app.css"
+import Sidebar from "./components/Sidebar/Sidebar"
+import Home from "./Pages/Home/Home"
 
 function App() {
   return (
     <div className="App">
-
+      <Topbar />
+      <div className="container">
+        <Sidebar/>
+        <Home/>
+      </div>
     </div>
   )
 }
