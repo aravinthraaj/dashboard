4756f5dd8a1fdf387ada60edbf3838dfb5059592
aravinthraaj <aravinthraajk@gmail.com>
2022-01-24 01:49:42 +0530

    UserList Folder created

---
+++ src/Pages/UserList/UserList.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./UserList.css"
+
+function UserList() {
+  return (
+    <div className="UserList">
+      <div>UserList</div>
+    </div>
+  )
+}
+
+export default UserList
